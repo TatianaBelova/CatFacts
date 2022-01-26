01c99109da753b76b15e30623ad5bd3968a15f3b
Татьяна <tatiana@MacBook-Air-Tatiana.local>
2022-01-26 18:40:41 +0400

    Initial commit

---
+++ app/src/main/java/com/example/catfacts/FactsListFragment.kt
@@ -0,0 +1,5 @@
+package com.example.catfacts
+
+class FactsListFragment extends Fragment {
+
+}
---
+++ app/src/main/java/com/example/catfacts/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.catfacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.catfacts.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+}
